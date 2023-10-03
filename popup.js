cd1ee0ba5c9d3f5d8c58afec6717d6200e4a1ce2
myxsan <yksl3461@gmail.com>
2023-10-03 23:00:30 +0300

    f

--- popup.js
+++ popup.js
@@ -1,12 +1,46 @@
+import { getActiveTabURL } from "./utils.js";
+
 // adding a new bookmark row to the popup
 const addNewBookmark = () => {};
 
-const viewBookmarks = () => {};
+const viewBookmarks = (currentBookmarks = []) => {
+  const bookmarksElement = document.getElementById("bookmarks");
+  bookmarksElement.innerHTML = "";
 
-const onPlay = e => {};
+  if (currentBookmarks.length > 0) {
+    for (let i = 0; i < currentBookmarks.length; i++) {
+      const bookmark = currentBookmarks[i];
+      addNewBookmark(bookmarksElement, bookmark);
+    }
+  } else {
+    bookmarksElement.innerHTML = '<i class="row">No bookmarks to show.</i>';
+  }
+};
 
-const onDelete = e => {};
+const onPlay = (e) => {};
 
-const setBookmarkAttributes =  () => {};
+const onDelete = (e) => {};
 
-document.addEventListener("DOMContentLoaded", () => {});
+const setBookmarkAttributes = () => {};
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const activeTab = await getActiveTabURL();
+  const queryParameters = activeTab.url.split("?")[1];
+  const urlParameters = new URLSearchParams(queryParameters);
+
+  const currentVideo = urlParameters.get("v");
+
+  if (activeTab.url.includes("youtube.com/watch") && currentVideo) {
+    chrome.storage.sync.get([currentVideo], (data) => {
+      const currentVideoBookmarks = data[currentVideo]
+        ? JSON.parse(data[currentVideo])
+        : [];
+
+      viewBookmarks(currentVideoBookmarks);
+    });
+  } else {
+    const container = document.getElementsByClassName("container")[0];
+    container.innerHTML =
+      '<div class="title">This is not a youtube video page.</div>';
+  }
+});
